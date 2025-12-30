ad4978c0694ad303514d4c55028badda0f6adf6c
parthmhatre20 <mhatre4268@gmail.com>
2025-12-31 04:22:40 +0530

    Fix: remove firebase_service import from services __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,5 @@
 from .speech_service import speech_service
 from .ai_service import ai_service
-from .firebase_service import firebase_service
 from .question_generator import question_generator_service
 
-__all__ = ['speech_service', 'ai_service', 'firebase_service', 'question_generator_service']
+__all__ = ['speech_service', 'ai_service', 'question_generator_service']
